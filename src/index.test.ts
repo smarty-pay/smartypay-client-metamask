25376f3e0005142e0cd292fd11eeab988fddfeca
edolganov <evgenij.dolganov@gmail.com>
2023-02-10 17:59:14 +0300

    Unit tests for get address method

---
+++ src/index.test.ts
@@ -0,0 +1,57 @@
+/*
+  SMARTy Pay Client Metamask
+  @author Evgeny Dolganov <evgenij.dolganov@gmail.com>
+*/
+
+
+import {SmartyPayMetamaskProvider} from './index';
+
+describe('SmartyPayMetamaskProvider', ()=>{
+
+  const validAddress = '0x14186C8215985f33845722730c6382443Bf9EC65';
+  const invalidAddress = validAddress.toLowerCase();
+
+  beforeEach(()=>{
+    // stub
+    (window as any).ethereum = {
+      request: jest.fn(),
+      on: jest.fn(),
+    }
+  });
+
+
+  describe('Web3Api', ()=>{
+
+    describe('getAddress', ()=> {
+
+      test('should convert to case-sensitive address format', async () => {
+
+        const api = SmartyPayMetamaskProvider.makeWeb3Api();
+        await api.connect();
+
+        // check with invalid address
+        (window as any).ethereum.request = ()=> [invalidAddress];
+        expect(await api.getAddress()).toBe(validAddress);
+
+        // check with valid address
+        (window as any).ethereum.request = ()=> [validAddress];
+        expect(await api.getAddress()).toBe(validAddress);
+
+      });
+    });
+  });
+
+  describe('hasWallet', ()=>{
+    test('should check window.ethereum', ()=>{
+
+      expect(SmartyPayMetamaskProvider.hasWallet()).toBe(true);
+
+      (window as any).ethereum = undefined;
+
+      expect(SmartyPayMetamaskProvider.hasWallet()).toBe(false);
+    });
+  })
+
+})
+
+
